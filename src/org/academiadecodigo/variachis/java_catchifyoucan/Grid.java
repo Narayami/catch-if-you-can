c52a65cc4fd739bd0a1f7701c94aa1813da0baee
Tiago Machado <tfamsoad@gmail.com>
2018-11-06 00:38:51 +0000

    updated grid - added constructor with grids and rows

---
+++ src/org/academiadecodigo/variachis/java_catchifyoucan/Grid.java
@@ -0,0 +1,17 @@
+package org.academiadecodigo.variachis.java_catchifyoucan;
+
+public class Grid {
+
+    private int cols;
+    private int rows;
+
+    private static final int CELLSIZE = 50; //TODO-SFX
+    private static final int PADDING = 10; //TODO-SFX
+
+    public Grid(int cols, int rows){
+
+        this.cols = cols;
+        this.rows = rows;
+
+    }
+}
